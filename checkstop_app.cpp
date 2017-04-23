a5c2dd2cfe52b11f24ce64c7042a29985d3ea106
Marri Devender Rao <devenrao@in.ibm.com>
2017-04-23 13:52:17 -0500

    Add code to report error in debug data collector
    Change-Id: Ic436e57318392e91ce1003417939bd5a90ae10f2
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- checkstop_app.cpp
+++ checkstop_app.cpp
@@ -1,5 +1,12 @@
+#include <phosphor-logging/elog.hpp>
+#include "elog-errors.hpp"
+
 int main(int argc, char* argv[])
 {
+    using namespace phosphor::logging;
+    using error = org::open_power::Host::Termination;
+    report<error>();
+
     return 0;
 }
 
