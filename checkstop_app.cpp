6c6329acd618388cb1688c48be6ed36c3c1a88d5
Marri Devender Rao <devenrao@in.ibm.com>
2017-04-23 13:52:17 -0500

    Add code to report error in checkstop app
    Change-Id: Ic436e57318392e91ce1003417939bd5a90ae10f2
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- checkstop_app.cpp
+++ checkstop_app.cpp
@@ -1,5 +1,12 @@
+#include <phosphor-logging/elog.hpp>
+#include "elog-errors.hpp"
+
 int main(int argc, char* argv[])
 {
+    using namespace phosphor::logging;
+    using error = org::open_power::Common::BMC::Checkstop;
+    report<error>(error::STRING("Checkstop"));
+
     return 0;
 }
 
