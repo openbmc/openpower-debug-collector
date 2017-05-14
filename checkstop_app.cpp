7974c9a425be0e7a6da8614590d719a80551730b
Marri Devender Rao <devenrao@in.ibm.com>
2017-05-14 09:11:05 -0500

    debug collector: fix error-log usage
    Log sbusplus exceptions instead of phosphor-logging
    error exceptions. Pick metadata structures from the
    phosphor-logging generated types.
    
    Resolves openbmc/openbmc#1469
    
    Change-Id: I9789edd80f3477eeb66b93dff6e12a95e89a0005
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- checkstop_app.cpp
+++ checkstop_app.cpp
@@ -1,10 +1,11 @@
 #include <phosphor-logging/elog.hpp>
-#include "elog-errors.hpp"
+#include <phosphor-logging/elog-errors.hpp>
+#include "org/open_power/Host/error.hpp"
 
 int main(int argc, char* argv[])
 {
     using namespace phosphor::logging;
-    using error = org::open_power::Host::Checkstop;
+    using error = sdbusplus::org::open_power::Host::Error::Checkstop;
     report<error>();
 
     return 0;
