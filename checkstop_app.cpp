2af635f04c41627db85ffa1a757504a8c4bbcb0c
Marri Devender Rao <devenrao@in.ibm.com>
2017-10-03 09:17:29 -0500

    DO NOT MERGE - Validate generation of elog-errors.hpp for new error
    Change-Id: Iad3336c71e9a3f59e194a93f2835d61e180eac1d
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- checkstop_app.cpp
+++ checkstop_app.cpp
@@ -1,6 +1,7 @@
 #include <phosphor-logging/elog.hpp>
 #include "phosphor-logging/elog-errors.hpp"
 #include "org/open_power/Host/error.hpp"
+#include "org/open_power/Test/error.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -8,6 +9,8 @@ int main(int argc, char* argv[])
     using error = sdbusplus::org::open_power::Host::Error::Checkstop;
     report<error>();
 
+    using testerror = sdbusplus::org::open_power::Test::Error::TestFailure;
+    report<testerror>();
     return 0;
 }
 
