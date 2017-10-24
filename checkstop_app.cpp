e418a4cda605302d5374491f7a7c9655136d2150
Marri Devender Rao <devenrao@in.ibm.com>
2017-10-24 02:27:15 -0500

    Renamed error yaml due to clash with error yaml in openpower-dbus
    To fix duplicate "Event" string in error message ""Message":
    "org.open_power.Error.Host.Event.Event"", renamed error yaml
    in openpower-dbus-interfaces from  org/open_power/Host/Event.errors.yaml
    to  org/open_power/Host.errors.yaml.
    
    Because of the above change there is a clash with the file names,
    as all the error yaml files are copied to the same location for
    generation of elog-errors.hpp by phosphor-logging.
    
    Renmaing openpower-debug-collector error yaml from
    org/open_power/Host.errors.yaml to  org/open_power/Debug.errors.yaml
    
    Change-Id: Ib5fc0cd176b5d9dc2f47f35b008277434855a711
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>

--- checkstop_app.cpp
+++ checkstop_app.cpp
@@ -1,11 +1,11 @@
 #include <phosphor-logging/elog.hpp>
 #include "phosphor-logging/elog-errors.hpp"
-#include "org/open_power/Host/error.hpp"
+#include "org/open_power/Debug/error.hpp"
 
 int main(int argc, char* argv[])
 {
     using namespace phosphor::logging;
-    using error = sdbusplus::org::open_power::Host::Error::Checkstop;
+    using error = sdbusplus::org::open_power::Debug::Error::Checkstop;
     report<error>();
 
     return 0;
