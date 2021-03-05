e2256e8997e4a0423465ba78d015b101f655c962
Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
2021-03-05 11:46:41 -0600

    openpower-debug-collector: Collect hostboot dump from each SBE
    This commit implements the dump create interface to collect the
    hostboot dump from each SBE.
    A dump entry will be created by requesting to
    phosphor-dump-manager, the collected files will be stored
    to a temporary path associated with the id.
    Scope of this commit is
    - Implement xyz::openbmc_project::Dump::Create to accept
    the dump parameters such as type of the dump and error log
    id.
    - Call phosphor dump manager to create an entry
    - Collect the dump from each SBE using sbe_dump chipop
    - Notify the phosphor dump manager once the dump is
    collected.
    For Hostboot dump:
    - Any error on collecting dump from primary processor
      will fail the dump collection
    - Dump collection errors on secondary processors  will
      be ignored.
    Tests:
    create dump using busctl and make sure the dump is collected
    in the path.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ia65142818ff7723721b78dae19f0d13afc1a33fc

---
+++ dump/dump.mk
@@ -0,0 +1,18 @@
+if DUMP_COLLECTION
+sbin_PROGRAMS += openpower-dump-manager
+
+openpower_dump_manager_SOURCES = dump/dump_manager.cpp \
+				dump/dump_manager_main.cpp \
+				dump/dump_utils.cpp
+
+openpower_dump_manager_LDFLAGS  =  \
+			$(PHOSPHOR_LOGGING_LIBS) \
+			$(SDBUSPLUS_LIBS) \
+			$(SDEVENTPLUS_LIBS) \
+			-lpdbg
+
+openpower_dump_manager_CXXFLAGS  = \
+			$(PHOSPHOR_LOGGING_CFLAGS) \
+			$(SDBUSPLUS_CFLAGS) \
+			$(SDEVENTPLUS_CFLAGS)
+endif
---
+++ dump/dump_utils.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <sdbusplus/server.hpp>
+
+#include <string>
+
+namespace openpower
+{
+namespace dump
+{
+namespace util
+{
+
+constexpr auto PROPERTY_INTF = "org.freedesktop.DBus.Properties";
+
+/**
+ * @brief Get DBUS service for input interface via mapper call
+ *
+ * @param[in] bus -  DBUS Bus Object
+ * @param[in] intf - DBUS Interface
+ * @param[in] path - DBUS Object Path
+ *
+ * @return distinct dbus name for input interface/path
+ **/
+std::string getService(sdbusplus::bus::bus& bus, const std::string& intf,
+                       const std::string& path);
+
+/**
+ *
+ * @param[in] interface - the interface the property is on
+ * @param[in] propertName - the name of the property
+ * @param[in] path - the D-Bus path
+ * @param[in] service - the D-Bus service
+ * @param[in] bus - the D-Bus object
+ * @param[in] value - the value to set the property to
+ */
+template <typename T>
+void setProperty(const std::string& interface, const std::string& propertyName,
+                 const std::string& path, const std::string& service,
+                 sdbusplus::bus::bus& bus, T& value)
+{
+    std::variant<T> propertyValue(value);
+    auto method = bus.new_method_call(service.c_str(), path.c_str(),
+                                      PROPERTY_INTF, "Set");
+    method.append(interface, propertyName, propertyValue);
+    auto reply = bus.call(method);
+}
+
+} // namespace util
+} // namespace dump
+} // namespace openpower
