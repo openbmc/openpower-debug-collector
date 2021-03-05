9441b9fbf15c82226957bec148acbf984afa882b
Dhruvaraj Subhshchandran <dhruvaraj@in.ibm.com>
2021-03-05 11:46:41 -0600

    Collect hostboot dump from each SBE
    This commit implements the dump create interface to collect the
    hostboot dump from each SBE.
    A dump entry will be created by requesting to
    phosphor-dump-manager, the collected files will be stored
    to a temporary path associated with the id.
    Scope of this commit is
    - Implement xyz::openbmc_project::Dump::Create to accept
    the dump parameters such as type of the dump and error log
    id.
    - Call phosphor dump manager to create an entry
    - Collect the dump from each SBE using sbe_dump chipop
    - Notify the phosphor dump manager once the dump is
    collected.
    For Hostboot dump:
    - Any error on collecting dump from primary processor
      will fail the dump collection
    - Dump collection errors on secondary processors  will
      be ignored.
    Tests:
    create dump using busctl and make sure the dump is collected
    in the path.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ia65142818ff7723721b78dae19f0d13afc1a33fc

---
+++ dump/dump_manager.hpp
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <libpdbg.h>
+
+#include <com/ibm/Dump/Create/server.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <sdeventplus/source/child.hpp>
+#include <sdeventplus/source/event.hpp>
+#include <xyz/openbmc_project/Dump/Create/server.hpp>
+
+#include <filesystem>
+
+namespace openpower
+{
+namespace dump
+{
+
+using CreateIface = sdbusplus::server::object::object<
+    sdbusplus::com::ibm::Dump::server::Create,
+    sdbusplus::xyz::openbmc_project::Dump::server::Create>;
+
+/** @struct DumpPtr
+ * @brief a structure holding the data pointer
+ * @details This is a RA11 containar for the dump data
+ * returned by the SBE
+ */
+struct DumpDataPtr
+{
+  public:
+    /** @brief Distructor for the object, free the allocated memory.
+     */
+    ~DumpDataPtr()
+    {
+        free(dataPtr);
+    }
+
+    /** @brief Returns the pointer to the data
+     */
+    uint8_t** getPtr()
+    {
+        return &dataPtr;
+    }
+
+    /** @brief Returns the stored data
+     */
+    uint8_t* getData()
+    {
+        return dataPtr;
+    }
+
+  private:
+    /** The pointer to the data
+     */
+    uint8_t* dataPtr = NULL;
+};
+
+/** @class Manager
+ *  @brief Dump  manager class
+ *  @details A concrete implementation for the
+ *  xyz::openbmc_project::Dump::server::Create D-Bus API.
+ */
+class Manager : public CreateIface
+{
+  public:
+    Manager() = delete;
+    Manager(const Manager&) = default;
+    Manager& operator=(const Manager&) = delete;
+    Manager(Manager&&) = delete;
+    Manager& operator=(Manager&&) = delete;
+    virtual ~Manager() = default;
+
+    /** @brief Constructor to put object onto bus at a dbus path.
+     *  @param[in] bus - Bus to attach to.
+     *  @param[in] path - Path of the service.
+     *  @param[in] event - sd event handler.
+     */
+    Manager(sdbusplus::bus::bus& bus, const char* path,
+            sdeventplus::Event& event) :
+        CreateIface(bus, path, true),
+        bus(bus), event(event)
+    {}
+
+    /** @brief Implementation for createDump
+     *  Method to request a host dump when there is an error related to host.
+     *  @param[in] params - Parameters for creating the dump.
+     *
+     *  @return object_path - The object path of the new dump entry.
+     */
+    sdbusplus::message::object_path
+        createDump(std::map<std::string, std::string> params) override;
+
+  private:
+    /** @brief sdbusplus DBus bus connection. */
+    sdbusplus::bus::bus& bus;
+
+    /** @brief sdbusplus Dump event loop */
+    sdeventplus::Event& event;
+
+    /** @brief SDEventPlus child pointer added to event loop */
+    std::unique_ptr<sdeventplus::source::Child> childPtr = nullptr;
+
+    /** @brief Method to find whether a processor is master
+     *  @param[in] proc - pdbg_target for processor target
+     *
+     *  @return bool - true if master processor else false.
+     */
+    bool isMasterProc(struct pdbg_target* proc) const;
+
+    /** @brief The function to orchestrate dump collection from different
+     *  sources
+     *  @param type - Type of the dump
+     *  @param id - A unique id assigned to dump to be collected
+     *  @param errorLogId - Id of the error log associated with this collection
+     */
+    void collectDump(uint8_t type, uint32_t id, std::string errorLogId);
+
+    /** @brief The function to collect dump from SBE
+     *  @param[in] proc - pdbg_target of the SBE to collect the dump.
+     *  @param[in] dumpPath - Path of directory to write the dump files.
+     *  @param[in] id - Id of the dump
+     *  @param[in] type - Type of the dump
+     *  @param[in] clockState - State of the clock while collecting.
+     */
+    void collectDumpFromSBE(struct pdbg_target* proc,
+                            std::filesystem::path& dumpPath, uint32_t id,
+                            uint8_t type, uint8_t clockState);
+};
+
+} // namespace dump
+} // namespace openpower
