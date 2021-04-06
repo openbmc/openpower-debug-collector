6617e9da04410ccbb68ec8d52a50d4735a2eb711
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-04-06 01:32:47 -0500

    Make GEN_ERRORS optional
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I58bb9ae9fe37801f6f5b97fadcb7b1099336dd2a

--- watchdog_timeout.cpp
+++ watchdog_timeout.cpp
@@ -1,13 +1,15 @@
 #include "org/open_power/Host/Boot/error.hpp"
-#include "phosphor-logging/elog-errors.hpp"
 
+#include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/log.hpp>
 
 int main(int argc, char* argv[])
 {
     using namespace phosphor::logging;
     using error =
         sdbusplus::org::open_power::Host::Boot::Error::WatchdogTimedOut;
+    log<level::ERR>("Watchdog timeout");
     report<error>();
 
     return 0;
