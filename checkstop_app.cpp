e69c04bd1b5459df3c92bd6f5c97082750097734
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-04-06 01:32:47 -0500

    Make GEN_ERRORS optional
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I58bb9ae9fe37801f6f5b97fadcb7b1099336dd2a

--- checkstop_app.cpp
+++ checkstop_app.cpp
@@ -1,12 +1,14 @@
 #include "org/open_power/Host/Boot/error.hpp"
-#include "phosphor-logging/elog-errors.hpp"
 
+#include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/log.hpp>
 
 int main(int argc, char* argv[])
 {
     using namespace phosphor::logging;
     using error = sdbusplus::org::open_power::Host::Boot::Error::Checkstop;
+    log<level::ERR>("A checkstop");
     report<error>();
 
     return 0;
