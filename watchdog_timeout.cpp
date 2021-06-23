b2064f98af36eb3c592779b385b5eb2d7dab9a5f
Shantappa Teekappanavar <sbteeks@yahoo.com>
2021-06-22 19:07:29 -0500

    watchdog: Collect hostboot dump when watchdog times out
    The hostboot dump collection is disabled by default and
    just the error is logged. To enable the dump collection,
    the meson option 'hostboot-dump-collection' must be enabled.
    
    Testing - with meson option 'hostboot-dump-collection' enabled:
    Ran watchdog_timeout:
    case-1: CurrentHostState - off, AutoReboot - false
    - Verified PEL object was not created
    - Verified hostboot dump was not created
    - Verified the Host State changed to Quiesce
    
    case-2: CurrentHostState - off, AutoReboot - true
    - Verified PEL object was created
    - Verified hostboot dump was not created
    - Verified the Host State changed to Running
    
    case-3: CurrentHostState - Running, AutoBoot - false
    - Verified PEL object was not created
    - Verified hostboot dump was not created
    - Verified the Host State changed to Quiesce
    
    case-4: CurrentHostState - Running, AutoBoot - true, default timeout = 300s
    - Verified PEL object was created
    - Verified hostboot dump was created
    - Observed Host state moving to either Running or Quiesce
    
    case-5: CurrentHostState - Running, AutoBoot - true, specified timeout = 5s
    - Verified PEL object was created
    - Verified hostboot dump was created
    - Observed Host state moving to either Running or Quiesce
    
    Docker Unit test: passed
    
    Signed-off-by: Shantappa Teekappanavar <sbteeks@yahoo.com>
    Change-Id: Ib92d0c2f282816fb742cf07c1cb876b2cc093c12

--- watchdog_timeout.cpp
+++ watchdog_timeout.cpp
@@ -1,14 +1,37 @@
 #include "org/open_power/Host/Boot/error.hpp"
 #include "phosphor-logging/elog-errors.hpp"
 
-#include <phosphor-logging/elog.hpp>
+#include <config.h>
 
-int main(int /*argc*/, char** /*argv*/)
+#include <CLI/CLI.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <watchdog/watchdog_main.hpp>
+
+int main(int argc, char* argv[])
 {
-    using namespace phosphor::logging;
-    using error =
-        sdbusplus::org::open_power::Host::Boot::Error::WatchdogTimedOut;
-    report<error>();
+    CLI::App app{"Hostboot dump collection"};
+
+    // Use default 1500 seconds timeout if dump progress status
+    // remains in 'InProgress' state.
+    constexpr uint32_t timeoutValue{1500};
+    uint32_t timeoutInterval{timeoutValue};
+    app.add_option("-t,--timeout", timeoutInterval,
+                   "Set timeout interval for watchdog timeout in seconds");
+
+    CLI11_PARSE(app, argc, argv);
+
+    if constexpr (hbdumpCollectionEnabled)
+    {
+        using namespace watchdog::dump;
+        triggerHostbootDump(timeoutInterval);
+    }
+    else
+    {
+        using namespace phosphor::logging;
+        using error =
+            sdbusplus::org::open_power::Host::Boot::Error::WatchdogTimedOut;
+        report<error>();
+    }
 
     return 0;
 }
