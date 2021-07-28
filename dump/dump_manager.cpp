be04b7a6412ab03acb24852d4485de998550af56
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-07-28 08:50:47 -0500

    openpower-debug-collector: Dump collection lock
    The hostboot and hardware dump collection involves
    collecting dump with clocks on and clocks off
    once clocks are off, another usable dump cannot be
    collected until next host reboot. This commit adds a
    file based mechanism to check whether a dump collection
    is in progress or completed.
    
    Steps:
       - The lock file will be cleared during host power on
       - Check for the file before continueing with collection
         - if file exists, do not allow the collection
         - otherwise create the file and continue collection
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I39853d4737c4392e40235f13bdd4750f9ca72eab

--- dump/dump_manager.cpp
+++ dump/dump_manager.cpp
@@ -405,6 +405,9 @@ sdbusplus::message::object_path Manager::createDump(DumpCreateParams params)
     using CreateParameters =
         sdbusplus::com::ibm::Dump::server::Create::CreateParameters;
     using Argument = xyz::openbmc_project::Common::InvalidArgument;
+    using NotAllowed =
+        sdbusplus::xyz::openbmc_project::Common::Error::NotAllowed;
+    using Reason = xyz::openbmc_project::Common::NotAllowed::REASON;
 
     auto iter = params.find(
         sdbusplus::com::ibm::Dump::server::Create::
@@ -522,6 +525,17 @@ sdbusplus::message::object_path Manager::createDump(DumpCreateParams params)
         }
     }
 
+    // Check whether a dump is in progresss or already collected
+    if (checkAndLockDumpCollection())
+    {
+        log<level::ERR>(
+            "A dump is already is in progress or collected in this boot, "
+            "another dump cannot be collected until host reboot");
+        elog<NotAllowed>(
+            Reason("A dump is already is in progress or collected in this "
+                   "boot, another dump cannot be collected until host reboot"));
+    }
+
     try
     {
         auto dumpManager =
@@ -631,6 +645,28 @@ sdbusplus::message::object_path Manager::createDump(DumpCreateParams params)
     }
     // return object path
     return newDumpPath;
+}
+
+bool Manager::checkAndLockDumpCollection()
+{
+    if (std::filesystem::exists(OP_DUMP_LOCK_FILE))
+    {
+        return true;
+    }
+
+    try
+    {
+        std::ofstream(OP_DUMP_LOCK_FILE);
+    }
+    catch (std::ofstream::failure& e)
+    {
+        log<level::ERR>(
+            fmt::format("Unable to create lock file({}) errorMsg({})",
+                        OP_DUMP_LOCK_FILE, e.what())
+                .c_str());
+        throw std::runtime_error("Unable to create dump lock file");
+    }
+    return false;
 }
 } // namespace dump
 } // namespace openpower
