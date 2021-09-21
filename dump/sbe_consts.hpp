4923f6b24b8ee1d5aa7fb8794afc14f03f34bdcb
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-09-20 20:25:51 -0500

    Add support for collecting Self Boot Engine(SBE) dump
    Self Boot Engine(SBE) is a microcontroller that sits inside the processor
    to initialize it to start the booting and also acts as a secure channel
    for accessing certain control functions on the processor. During the
    booting or other hardware access operations SBE can encounter errors
    and become unresponsive. In such situations, the debug data needs to be
    collected from such SBEs to find out the root cause of the error.
    This data includes hardware state, configuration, memory, etc.
    The collected data is then packaged into the OpenPOWER dump format
    and which is called as SBE dump.
    
    The SBE dump is collected from a dead SBE using special procedures
    which execute hardware access instructions to get the memory
    and other hardware states. This commit add support for calling
    such procdure implementations.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I020406b2d53d9d1f0a677a7defc4e55009c5e266

--- dump/sbe_consts.hpp
+++ dump/sbe_consts.hpp
@@ -8,6 +8,10 @@ namespace SBE
 // Dump type to the sbe_dump chipop
 constexpr auto SBE_DUMP_TYPE_HOSTBOOT = 0x5;
 constexpr auto SBE_DUMP_TYPE_HARDWARE = 0x1;
+
+// SBE dump type
+constexpr auto SBE_DUMP_TYPE_SBE = 0xA;
+
 // Clock state requested
 // Collect the dump with clocks on
 constexpr auto SBE_CLOCK_ON = 0x1;
