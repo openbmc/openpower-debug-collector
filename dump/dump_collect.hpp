44dd2a0195f5a522da705b8b977213d7cbe83f9f
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-09-20 20:25:51 -0500

    Add support for collecting Self Boot Engine(SBE) dump
    SBE is a microcontroller that sits inside the processor
    to initialize it to start the booting and also acts as a secure channel
    for accessing certain control functions on the processor. During the
    booting or other hardware access operations SBE can encounter errors
    and become unresponsive. In such situations, the debug data needs to be
    collected from such SBEs to find out the root cause of the error.
    This data includes hardware state, configuration, memory, etc.
    The collected data is then packaged into the OpenPOWER dump format
    and which is called as SBE dump.
    
    The SBE dump is collected from an responsive SBE using special procedures
    which execute hardware access instructions to get the memory
    and other hardware states. This commit add support for calling
    such procedure implementations.
    
    Test:
     busctl --verbose call org.open_power.Dump.Manager  /org/openpower/dump xyz.openbmc_project.Dump.Create CreateDump a{sv} 3 "com.ibm.Dump.Create.CreateParameters.DumpType" s "com.ibm.Dump.Create.DumpType.SBE" "com.ibm.Dump.Create.CreateParameters.ErrorLogId" t 0xDEADBEEF "com.ibm.Dump.Create.CreateParameters.FailingUnitId" t 1
    MESSAGE "o" {
            OBJECT_PATH "/xyz/openbmc_project/dump/sbe/entry/3";
    };
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I020406b2d53d9d1f0a677a7defc4e55009c5e266

---
+++ dump/dump_collect.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+extern "C"
+{
+#include <libpdbg.h>
+}
+
+#include <filesystem>
+
+namespace openpower
+{
+namespace dump
+{
+
+namespace sbe_chipop
+{
+/** @brief The function to orchestrate dump collection from different
+ *  SBEs
+ *  @param type - Type of the dump
+ *  @param id - A unique id assigned to dump to be collected
+ *  @param failingUnit - Chip position of the failing unit
+ *  @param sbeFilePath - Path where the collected dump to be stored
+ */
+void collectDump(const uint8_t type, const uint32_t id,
+                 const uint64_t failingUnit, const std::filesystem::path& path);
+
+/** @brief The function to collect dump from SBE
+ *  @param[in] proc - pdbg_target of the proc containing SBE to collect the
+ * dump.
+ *  @param[in] dumpPath - Path of directory to write the dump files.
+ *  @param[in] id - Id of the dump
+ *  @param[in] type - Type of the dump
+ *  @param[in] clockState - State of the clock while collecting.
+ *  @param[in] chipPos - Position of the chip
+ *  @param[in] collectFastArray - 0: skip fast array collection 1: collect
+ * fast array
+ */
+void collectDumpFromSBE(struct pdbg_target* proc,
+                        const std::filesystem::path& path, const uint32_t id,
+                        const uint8_t type, const uint8_t clockState,
+                        const uint8_t chipPos, const uint8_t collectFastArray);
+} // namespace sbe_chipop
+
+/** @brief create dump directories and add error log id
+ *  @param dumpPath Directory for collecting dump
+ *  @errorLogId Error log id associated with dump
+ */
+void prepareCollection(const std::filesystem::path& dumpPath,
+                       const std::string& errorLogId);
+
+} // namespace dump
+} // namespace openpower
--- dump/dump_manager.hpp
+++ dump/dump_manager.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <libpdbg.h>
-
 #include <com/ibm/Dump/Create/server.hpp>
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server/object.hpp>
@@ -21,41 +19,6 @@ using DumpCreateParams =
 using CreateIface = sdbusplus::server::object::object<
     sdbusplus::com::ibm::Dump::server::Create,
     sdbusplus::xyz::openbmc_project::Dump::server::Create>;
-
-/** @struct DumpPtr
- * @brief a structure holding the data pointer
- * @details This is a RAII container for the dump data
- * returned by the SBE
- */
-struct DumpDataPtr
-{
-  public:
-    /** @brief Destructor for the object, free the allocated memory.
-     */
-    ~DumpDataPtr()
-    {
-        // The memory is allocated using malloc
-        free(dataPtr);
-    }
-
-    /** @brief Returns the pointer to the data
-     */
-    uint8_t** getPtr()
-    {
-        return &dataPtr;
-    }
-
-    /** @brief Returns the stored data
-     */
-    uint8_t* getData()
-    {
-        return dataPtr;
-    }
-
-  private:
-    /** The pointer to the data */
-    uint8_t* dataPtr = NULL;
-};
 
 /** @class Manager
  *  @brief Dump  manager class
@@ -102,40 +65,6 @@ class Manager : public CreateIface
 
     /** @brief SDEventPlus child pointer added to event loop */
     std::unique_ptr<sdeventplus::source::Child> childPtr = nullptr;
-
-    /** @brief Method to find whether a processor is master
-     *  @param[in] proc - pdbg_target for processor target
-     *
-     *  @return bool - true if master processor else false.
-     */
-    bool isMasterProc(struct pdbg_target* proc) const;
-
-    /** @brief The function to orchestrate dump collection from different
-     *  SBEs
-     *  @param type - Type of the dump
-     *  @param id - A unique id assigned to dump to be collected
-     *  @param errorLogId - Id of the error log associated with this collection
-     *  @param failingUnit - Chip position of the failing unit
-     */
-    void collectDump(uint8_t type, uint32_t id, std::string errorLogId,
-                     const uint64_t failingUnit);
-
-    /** @brief The function to collect dump from SBE
-     *  @param[in] proc - pdbg_target of the proc conating SBE to collect the
-     * dump.
-     *  @param[in] dumpPath - Path of directory to write the dump files.
-     *  @param[in] id - Id of the dump
-     *  @param[in] type - Type of the dump
-     *  @param[in] clockState - State of the clock while collecting.
-     *  @param[in] chipPos - Position of the chip
-     *  @param[in] collectFastArray - 0: skip fast array collection 1: collect
-     * fast array
-     */
-    void collectDumpFromSBE(struct pdbg_target* proc,
-                            std::filesystem::path& dumpPath, uint32_t id,
-                            uint8_t type, uint8_t clockState,
-                            const uint8_t chipPos,
-                            const uint8_t collectFastArray);
 };
 
 } // namespace dump
