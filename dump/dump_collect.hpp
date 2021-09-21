cde8fecdfa5563f59b006e4d92a8b4ccb1b6e2a3
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-09-20 20:25:51 -0500

    Add support for collecting Self Boot Engine(SBE) dump
    SBE is a microcontroller that sits inside the processor
    to initialize it to start the booting and also acts as a secure channel
    for accessing certain control functions on the processor. During the
    booting or other hardware access operations SBE can encounter errors
    and become unresponsive. In such situations, the debug data needs to be
    collected from such SBEs to find out the root cause of the error.
    This data includes hardware state, configuration, memory, etc.
    The collected data is then packaged into the OpenPOWER dump format
    and which is called as SBE dump.
    
    The SBE dump is collected from an responsive SBE using special procedures
    which execute hardware access instructions to get the memory
    and other hardware states. This commit add support for calling
    such procedure implementations.
    
    Test:
     busctl --verbose call org.open_power.Dump.Manager  /org/openpower/dump xyz.openbmc_project.Dump.Create CreateDump a{sv} 3 "com.ibm.Dump.Create.CreateParameters.DumpType" s "com.ibm.Dump.Create.DumpType.SBE" "com.ibm.Dump.Create.CreateParameters.ErrorLogId" t 0xDEADBEEF "com.ibm.Dump.Create.CreateParameters.FailingUnitId" t 1
    MESSAGE "o" {
            OBJECT_PATH "/xyz/openbmc_project/dump/sbe/entry/3";
    };
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I020406b2d53d9d1f0a677a7defc4e55009c5e266

---
+++ dump/dump_collect.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <libpdbg.h>
+
+#include <filesystem>
+
+namespace openpower
+{
+namespace dump
+{
+
+/** @struct DumpPtr
+ * @brief a structure holding the data pointer
+ * @details This is a RAII container for the dump data
+ * returned by the SBE
+ */
+struct DumpDataPtr
+{
+  public:
+    /** @brief Destructor for the object, free the allocated memory.
+     */
+    ~DumpDataPtr()
+    {
+        // The memory is allocated using malloc
+        free(dataPtr);
+    }
+
+    /** @brief Returns the pointer to the data
+     */
+    uint8_t** getPtr()
+    {
+        return &dataPtr;
+    }
+
+    /** @brief Returns the stored data
+     */
+    uint8_t* getData()
+    {
+        return dataPtr;
+    }
+
+  private:
+    /** The pointer to the data */
+    uint8_t* dataPtr = NULL;
+};
+
+namespace sbe_chipop
+{
+/** @brief The function to orchestrate dump collection from different
+ *  SBEs
+ *  @param type - Type of the dump
+ *  @param id - A unique id assigned to dump to be collected
+ *  @param failingUnit - Chip position of the failing unit
+ *  @param sbeFilePath - Path where the collected dump to be stored
+ */
+void collectDump(uint8_t type, uint32_t id, const uint64_t failingUnit,
+                 std::filesystem::path& sbeFilePath);
+
+/** @brief The function to collect dump from SBE
+ *  @param[in] proc - pdbg_target of the proc conating SBE to collect the
+ * dump.
+ *  @param[in] dumpPath - Path of directory to write the dump files.
+ *  @param[in] id - Id of the dump
+ *  @param[in] type - Type of the dump
+ *  @param[in] clockState - State of the clock while collecting.
+ *  @param[in] chipPos - Position of the chip
+ *  @param[in] collectFastArray - 0: skip fast array collection 1: collect
+ * fast array
+ */
+void collectDumpFromSBE(struct pdbg_target* proc,
+                        std::filesystem::path& dumpPath, uint32_t id,
+                        uint8_t type, uint8_t clockState, const uint8_t chipPos,
+                        const uint8_t collectFastArray);
+} // namespace sbe_chipop
+
+/** @brief Method to find whether a processor is master
+ *  @param[in] proc - pdbg_target for processor target
+ *
+ *  @return bool - true if master processor else false.
+ */
+bool isMasterProc(struct pdbg_target* proc);
+
+/** @brief create dump directories and add error log id
+ *  @param dumpPath Directory for collecting dump
+ *  @param collectionPath sub directory to store files
+ *  @errorLogId Error log id associated with dump
+ */
+void prepareCollection(const std::filesystem::path dumpPath,
+                       std::string collectionPath, std::string errorLogId);
+
+} // namespace dump
+} // namespace openpower
