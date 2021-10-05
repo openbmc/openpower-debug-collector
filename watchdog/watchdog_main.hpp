87cb14614ef5f18b68b147e52e6439430f867ef4
Shantappa Teekappanavar <sbteeks@yahoo.com>
2021-10-05 12:17:55 -0500

    Collect SBE dump when watchdog times out
    Added support to handle SBE boot failure when watchdog
    times out in the SBE boot window. FFDC information from SBE
    is captured using libphal provided API, and the SBE specific
    PEL for a valid FFDC is created. In case the error is related
    to SBE timeout or no FFDC data then SBE dump to capture
    additional debug data is initiated.
    
    Tested: verified PEL log
    root@p10bmc:~# peltool -l
    {
        "0x50000332": {
            "SRC":                  "BD123504",
            "Message":              "timeout reported during SBE boot
    process",
            "PLID":                 "0x50000332",
            "CreatorID":            "BMC",
            "Subsystem":            "Processor Chip Cache",
            "Commit Time":          "10/04/2021 18:25:27",
            "Sev":                  "Unrecoverable Error",
            "CompID":               "0x3500"
        }
    }
    
    Signed-off-by: Shantappa Teekappanavar <sbteeks@yahoo.com>
    Change-Id: Ibfe7cc6619cd99f303c6106e617bc636632d0940

--- watchdog/watchdog_main.hpp
+++ watchdog/watchdog_main.hpp
@@ -19,5 +19,13 @@ namespace dump
  */
 void triggerHostbootDump(const uint32_t timeout);
 
+/**
+ * @brief Initiate SBE dump collection
+ *
+ * @param procTarget - Processor target
+ * @param timeout - timeout interval in seconds
+ */
+void triggerSBEDump(struct pdbg_target* procTarget, const uint32_t timeout);
+
 } // namespace dump
 } // namespace watchdog
