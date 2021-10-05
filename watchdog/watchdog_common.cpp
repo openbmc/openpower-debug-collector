9229a61c88ede7d4ceb561b24d32637a1e7928b1
Shantappa Teekappanavar <sbteeks@yahoo.com>
2021-10-05 12:17:55 -0500

    Collect SBE dump when watchdog times out
    Added support to handle SBE boot failure when watchdog
    times out in the SBE boot window. FFDC information from SBE
    is captured using libphal provided API, and the SBE specific
    PEL for a valid FFDC is created. In case the error is related
    to SBE timeout or no FFDC data then SBE dump to capture
    additional debug data is initiated.
    
    Tested: verified PEL log
    root@p10bmc:~# peltool -l
    {
        "0x50000332": {
            "SRC":                  "BD123504",
            "Message":              "timeout reported during SBE boot
    process",
            "PLID":                 "0x50000332",
            "CreatorID":            "BMC",
            "Subsystem":            "Processor Chip Cache",
            "Commit Time":          "10/04/2021 18:25:27",
            "Sev":                  "Unrecoverable Error",
            "CompID":               "0x3500"
        }
    }
    
    Signed-off-by: Shantappa Teekappanavar <sbteeks@yahoo.com>
    Change-Id: Ibfe7cc6619cd99f303c6106e617bc636632d0940

--- watchdog/watchdog_common.cpp
+++ watchdog/watchdog_common.cpp
@@ -1,4 +1,6 @@
-#include <libpdbg.h>
+#include "attributes_info.H"
+
+#include <fmt/format.h>
 
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/bus.hpp>
@@ -60,6 +62,79 @@ bool isAutoRebootEnabled()
     }
 
     return autoReboot;
+}
+
+bool isPrimaryProc(struct pdbg_target* procTarget)
+{
+    ATTR_PROC_MASTER_TYPE_Type type;
+
+    // Get processor type (Primary or Secondary)
+    if (DT_GET_PROP(ATTR_PROC_MASTER_TYPE, procTarget, type))
+    {
+        log<level::ERR>("Attribute [ATTR_PROC_MASTER_TYPE] get failed");
+        throw std::runtime_error(
+            "Attribute [ATTR_PROC_MASTER_TYPE] get failed");
+    }
+
+    bool primaryProc = false;
+    /* Attribute value 0 corresponds to primary processor */
+    if (type == ENUM_ATTR_PROC_MASTER_TYPE_ACTING_MASTER)
+    {
+        primaryProc = true;
+    }
+    return primaryProc;
+}
+
+void setDevtreeEnv()
+{
+    // PDBG_DTB environment variable set to CEC device tree path
+    static constexpr auto PDBG_DTB_PATH =
+        "/var/lib/phosphor-software-manager/pnor/rw/DEVTREE";
+
+    if (setenv("PDBG_DTB", PDBG_DTB_PATH, true))
+    {
+        log<level::ERR>(
+            fmt::format("Failed to set PDBG_DTB: ({})", strerror(errno))
+                .c_str());
+        throw std::runtime_error("Failed to set PDBG_DTB");
+    }
+}
+
+struct pdbg_target* getPibTarget(struct pdbg_target* procTarget)
+{
+    char path[16];
+    sprintf(path, "/proc%d/pib", pdbg_target_index(procTarget));
+    struct pdbg_target* pib = pdbg_target_from_path(nullptr, path);
+    if (pib == nullptr)
+    {
+        log<level::ERR>(fmt::format("Failed to get PIB target for: {}",
+                                    pdbg_target_path(procTarget))
+                            .c_str());
+        throw std::runtime_error("Failed to get PIB target");
+    }
+
+    // Probe the target
+    if (pdbg_target_probe(pib) != PDBG_TARGET_ENABLED)
+    {
+        log<level::ERR>(fmt::format("Failed to enable PIB target: {}",
+                                    pdbg_target_path(pib))
+                            .c_str());
+        throw std::runtime_error("Failed to enable PIB target");
+    }
+    return pib;
+}
+
+void pdbgInit()
+{
+    // PDBG_DTB environment variable set to CEC device tree path
+    setDevtreeEnv();
+
+    log<level::INFO>("pdbgInit(): Initialize pdbg targets");
+    if (!pdbg_targets_init(nullptr))
+    {
+        log<level::ERR>("pdbg_targets_init failed");
+        throw std::runtime_error("pdbg target initialization failed");
+    }
 }
 
 } // namespace dump
--- watchdog/watchdog_common.hpp
+++ watchdog/watchdog_common.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <string>
+extern "C"
+{
+#include <libpdbg.h>
+}
 
 namespace watchdog
 {
@@ -16,6 +20,39 @@ void transitionHost(const std::string& target);
 
 /** @brief Read state of autoreboot property via dbus */
 bool isAutoRebootEnabled();
+
+/**
+ *  @brief  Check if primary processor or not
+ *
+ *  @param procTarget - Processor target to check if primary or not
+ *
+ *  @return True/False
+ */
+bool isPrimaryProc(struct pdbg_target* procTarget);
+
+/**
+ * @brief Helper function to set PDBG_DTB
+ *
+ * PDBG_DTB environment variable set to CEC device tree path
+ */
+void setDevtreeEnv();
+
+/** @brief initialize pdbg target
+ *
+ * Exceptions: runtime_error
+ */
+void pdbgInit();
+
+/**
+ *  @brief  Helper function to find PIB target needed for PIB operations
+ *
+ *  @param[in]  procTarget - Processor target to find the PIB target on
+ *
+ *  @return Valid pointer to PIB target on success
+ *
+ *  Exceptions: runtime_error
+ */
+struct pdbg_target* getPibTarget(struct pdbg_target* procTarget);
 
 } // namespace dump
 } // namespace watchdog
