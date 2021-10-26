945c615884208ea27408f9ff322168cbb2098086
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-26 09:36:16 -0500

    Make dump collection a stand-alone app
    When forking from the openpower-dump-collector a D-Bus
    service, there is error in getting the bus. So moved
    the collection part as a stand alone application
    
    Test Result:
    Inject error on SBE
    start hostboot dump
    
    dump-collect[11152]: Collecting dump type(5), clockstate(1), proc position(0)
    dump-collect[11153]: Collecting dump type(5), clockstate(1), proc position(1)
    dump-collect[11154]: Collecting dump type(5), clockstate(1), proc position(2)
    dump-collect[11155]: Collecting dump type(5), clockstate(1), proc position(3)
    dump-collect[11153]: Dump collected type(5), clockstate(1), position(1)
    dump-collect[11155]: Dump collected type(5), clockstate(1), position(3)
    dump-collect[11154]: Dump collected type(5), clockstate(1), position(2)
    openpower-dump-manager[11152]: sbe dump() returned rc=110
    dump-collect[11152]: Error in collecting dump dump type(5), clockstate(1), proc position(0), collectFastArray(0) error(SBE command timeout)
    kernel: sbefifo 00:00:00:06: FIFO reset timed out
    phosphor-log-manager[322]: Created PEL 0x500b9b93 (BMC ID 4954) with SRC BD123501
    dump-collect[11152]: Requesting Dump PEL(1342938003) chip position(0)
    phosphor-dump-manager[418]: OpenPOWER dumps are enabled
    dump-collect[11152]: dump requested (waiting)
    Collecting SBE dump: path=/tmp/openpower-dumps/sbe/23/plat_dump, id=23, chip position=0
    Dump status(xyz.openbmc_project.Common.Progress.OperationStatus.Completed) : path=/xyz/openbmc_project/dump/sbe/entry/23
    Hostboot dump collection failed on primary, aborting colllection
    Dump collection failed, status(256) pid(11152)
    Failed to collect the dump
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I916b75a8a63daffcd8337b5e62f7db25b6b256a7

---
+++ dump/dump_collect_main.cpp
@@ -0,0 +1,59 @@
+#include "argument.hpp"
+#include "dump_collect.hpp"
+#include "sbe_consts.hpp"
+
+#include <iostream>
+#include <string>
+
+static void ExitWithError(const char* err, char** argv)
+{
+    openpower::dump::util::ArgumentParser::usage(argv);
+    std::cerr << std::endl;
+    std::cerr << "ERROR: " << err << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char** argv)
+{
+    // Read arguments.
+    auto options = openpower::dump::util::ArgumentParser(argc, argv);
+
+    // Parse arguments
+    std::string typeStr = std::move((options)["type"]);
+    if (typeStr.empty())
+    {
+        ExitWithError("type not specified", argv);
+    }
+
+    auto type = std::stoi(typeStr);
+    if (!((type == openpower::dump::SBE::SBE_DUMP_TYPE_HOSTBOOT) ||
+          (type == openpower::dump::SBE::SBE_DUMP_TYPE_HARDWARE)))
+    {
+        ExitWithError("type specified is invalid.", argv);
+    }
+
+    std::string idStr = std::move((options)["id"]);
+    if (idStr.empty())
+    {
+        ExitWithError("Dump id is not provided", argv);
+    }
+    auto id = std::stoi(idStr);
+
+    std::filesystem::path path = std::move((options)["path"]);
+    if (path.empty())
+    {
+        ExitWithError("Collection path not specified.", argv);
+    }
+
+    std::string failingUnitStr = std::move((options)["failingunit"]);
+    auto failingUnit = std::stoi(failingUnitStr);
+    try
+    {
+        openpower::dump::sbe_chipop::collectDump(type, id, failingUnit, path);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
