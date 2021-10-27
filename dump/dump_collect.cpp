6c2c052cb0987a2293981d8218afddff6380eb3a
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-27 06:43:29 -0500

    Add support for collecting dump from SBE
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ifa818e832e0fad2598187ff9575e2a9b798ee357

--- dump/dump_collect.cpp
+++ dump/dump_collect.cpp
@@ -1,8 +1,14 @@
+extern "C"
+{
+#include <libpdbg_sbe.h>
+}
 #include "dump_collect.hpp"
 
 #include <fmt/core.h>
+#include <libphal.H>
 
 #include <phosphor-logging/log.hpp>
+#include <sbe_consts.hpp>
 
 #include <cstdint>
 #include <filesystem>
@@ -14,6 +20,20 @@ namespace dump
 namespace sbe_chipop
 {
 
+void collectDumpFromSBE(struct pdbg_target* proc,
+                        const std::filesystem::path& path, const uint32_t id,
+                        const uint8_t type, const uint8_t clockState,
+                        const uint64_t failingUnit)
+{
+    using namespace phosphor::logging;
+    auto chipPos = pdbg_target_index(proc);
+    log<level::INFO>(fmt::format("Collect dump from proc({}) path({}) id({}) "
+                                 "type({}) clock({}) failingUnit({})",
+                                 chipPos, path.string(), id, type, clockState,
+                                 failingUnit)
+                         .c_str());
+}
+
 void collectDump(const uint8_t type, const uint32_t id,
                  const uint64_t failingUnit, const std::filesystem::path& path)
 {
@@ -23,6 +43,84 @@ void collectDump(const uint8_t type, const uint32_t id,
             "Dump collection started type({}) id({}) failingUnit({}), path({})",
             type, id, failingUnit, path.string())
             .c_str());
+    struct pdbg_target* target = nullptr;
+    auto failed = false;
+    // Initialize PDBG
+    openpower::phal::pdbg::init();
+    std::vector<uint8_t> clockStates = {SBE::SBE_CLOCK_ON, SBE::SBE_CLOCK_OFF};
+    for (auto cstate : clockStates)
+    {
+        std::vector<pid_t> pidList;
+        pdbg_for_each_class_target("proc", target)
+        {
+            if (pdbg_target_probe(target) != PDBG_TARGET_ENABLED)
+            {
+                continue;
+            }
+            if (!openpower::phal::pdbg::isTgtFunctional(target))
+            {
+                if (openpower::phal::pdbg::isPrimaryProc(target))
+                {
+                    // Primary processor is not functional
+                    log<level::INFO>("Primary Processor is not functional");
+                }
+                continue;
+            }
+
+            pid_t pid = fork();
+            if (pid < 0)
+            {
+                log<level::ERR>(
+                    "Fork failed while starting hostboot dump collection");
+                throw std::runtime_error(
+                    "Fork failed while starting hostboot dump collection");
+            }
+            else if (pid == 0)
+            {
+                try
+                {
+                    collectDumpFromSBE(target, path, id, type, cstate,
+                                       failingUnit);
+                }
+                catch (const std::runtime_error& error)
+                {
+                    log<level::ERR>(
+                        fmt::format(
+                            "Failed to execute collection, errorMsg({})",
+                            error.what())
+                            .c_str());
+                    std::exit(EXIT_FAILURE);
+                }
+                std::exit(EXIT_SUCCESS);
+            }
+            else
+            {
+                pidList.push_back(std::move(pid));
+            }
+        }
+        for (auto& p : pidList)
+        {
+            auto status = 0;
+            waitpid(p, &status, 0);
+            if (WEXITSTATUS(status))
+            {
+                log<level::ERR>(
+                    fmt::format("Dump collection failed, status({}) pid({})",
+                                status, p)
+                        .c_str());
+                failed = true;
+            }
+        }
+        // Exit if there is a critical failure and collection cannot continue
+        if (failed)
+        {
+            log<level::ERR>("Failed to collect the dump");
+            std::exit(EXIT_FAILURE);
+        }
+        log<level::INFO>(
+            fmt::format("Dump collection completed for clock_state({})", cstate)
+                .c_str());
+    }
 }
 } // namespace sbe_chipop
 } // namespace dump
