02dcbb21ba873b358fac39bf0a46212761f0cb59
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-27 03:26:06 -0500

    Add dump collection application
    Add a standalone application to execute the SBE chip-ops and
    collect the dumps. This standalone application is created
    to isolate the long-running dump collection from the D-Bus
    service,
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: If6f44075d33af20e09a442d7968d235dc6e8ea16

---
+++ dump/dump_collect_main.cpp
@@ -0,0 +1,84 @@
+#include "argument.hpp"
+#include "dump_collect.hpp"
+#include "sbe_consts.hpp"
+
+#include <filesystem>
+#include <iostream>
+
+namespace openpower
+{
+namespace dump
+{
+namespace util
+{
+
+static void ExitWithError(const std::string& err,
+                          const std::string& programName)
+{
+    ArgumentParser::usage(programName);
+    std::cerr << std::endl << "ERROR: " << err << std::endl;
+    std::exit(EXIT_FAILURE);
+}
+
+} // namespace util
+} // namespace dump
+} // namespace openpower
+
+int main(int argc, char** argv)
+{
+    using namespace openpower::dump::util;
+    using namespace openpower::dump::sbe_chipop;
+    using std::filesystem::path;
+    using namespace openpower::dump::SBE;
+
+    // Instantiate the argument parser
+    ArgumentParser options(argc, argv);
+
+    // Parse arguments
+    auto typeOpt = options.getInt("type");
+    if (!typeOpt.has_value())
+    {
+        ExitWithError("Type not specified", argv[0]);
+    }
+
+    auto type = typeOpt.value();
+    if (!(type == SBE_DUMP_TYPE_HOSTBOOT || type == SBE_DUMP_TYPE_HARDWARE))
+    {
+        ExitWithError("Type specified is invalid.", argv[0]);
+    }
+
+    auto idOpt = options.getInt("id");
+    if (!idOpt.has_value())
+    {
+        ExitWithError("Dump id is not provided", argv[0]);
+    }
+    auto id = idOpt.value();
+
+    auto pathOpt = options.getString("path");
+    if (!pathOpt.has_value())
+    {
+        ExitWithError("Collection path not specified.", argv[0]);
+    }
+    auto path = pathOpt.value();
+
+    auto failingUnitOpt = options.getInt("failingunit");
+    auto failingUnit = 0xFFFFFF; // Default or unspecified value
+    if (failingUnitOpt.has_value())
+    {
+        failingUnit = failingUnitOpt.value();
+    }
+
+    SbeDumpCollector dumpCollector;
+
+    try
+    {
+        dumpCollector.collectDump(type, id, failingUnit, path);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Failed to collect dump: " << e.what() << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
