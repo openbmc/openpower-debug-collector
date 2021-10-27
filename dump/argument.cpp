afb760b2ab47e8d7a196d8907f2042d77b4c80ed
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-27 03:06:44 -0500

    Add argument parser
    Add argument parser support to parse the arguments given to
    the dump collector application.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I19c618708485641a82d766eb99cb3f1b59f98c1b

---
+++ dump/argument.cpp
@@ -0,0 +1,81 @@
+#include "argument.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+namespace openpower
+{
+namespace dump
+{
+namespace util
+{
+
+ArgumentParser::ArgumentParser(int argc, char** argv)
+{
+    auto option = 0;
+    while (-1 != (option = getopt_long(argc, argv, optionstr, options, NULL)))
+    {
+        if ((option == '?') || (option == 'h'))
+        {
+            usage(argv);
+            exit(-1);
+        }
+
+        auto i = &options[0];
+        while ((i->val != option) && (i->val != 0))
+        {
+            ++i;
+        }
+
+        if (i->val)
+        {
+            arguments[i->name] = (i->has_arg ? optarg : true_string);
+        }
+    }
+}
+
+const std::string& ArgumentParser::operator[](const std::string& opt)
+{
+    auto i = arguments.find(opt);
+    if (i == arguments.end())
+    {
+        return empty_string;
+    }
+    else
+    {
+        return i->second;
+    }
+}
+
+void ArgumentParser::usage(char** argv)
+{
+    std::cerr << "Usage: " << argv[0] << " [options]\n";
+    std::cerr << "Options:\n";
+    std::cerr << "    --help            Print this menu\n";
+    std::cerr << "    --type            type of dump\n";
+    std::cerr
+        << "                      Valid types:0 - Hardware, 5 - Hostboot \n";
+    std::cerr << "    --id              Id of the dump\n";
+    std::cerr << "    --path            path to store dump\n";
+    std::cerr << "    --failingunit     Id of the failing unit\n";
+    std::cerr << std::flush;
+}
+
+const option ArgumentParser::options[] = {
+    {"type", required_argument, nullptr, 't'},
+    {"id", required_argument, nullptr, 'i'},
+    {"path", required_argument, nullptr, 'p'},
+    {"failingunit", required_argument, nullptr, 'f'},
+    {"help", no_argument, nullptr, 'h'},
+    {0, 0, 0, 0},
+};
+
+const char* ArgumentParser::optionstr = "tipfh?";
+
+const std::string ArgumentParser::true_string = "true";
+const std::string ArgumentParser::empty_string = "";
+
+} // namespace util
+} // namespace dump
+} // namespace openpower
