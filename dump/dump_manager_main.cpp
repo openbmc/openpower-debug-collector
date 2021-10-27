a1ddff90fe7ec7e54d942aaab675a6c94768c5fc
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-26 22:32:27 -0500

    Keep a dummy openpower dump manager service
    The dump collection interface is moved to phosphor-
    dump-manager but keeping openpower-dump-manager
    as a proxy of phosphor-dump-manager until the clients
    are moving to new interface for collecting host dumps.
    
    Change-Id: I9e5cd30dd1daadc6fa36fa9ce92293f7235566c6
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

---
+++ dump/dump_manager_main.cpp
@@ -0,0 +1,22 @@
+#include "config.h"
+
+#include "dump_manager.hpp"
+
+#include <sdbusplus/bus.hpp>
+int main()
+{
+    auto bus = sdbusplus::bus::new_default();
+
+    // Add sdbusplus ObjectManager for the 'root' path of the DUMP manager.
+    sdbusplus::server::manager::manager objManager(bus, OP_DUMP_OBJPATH);
+    bus.request_name(OP_DUMP_BUSNAME);
+
+    openpower::dump::Manager mgr(bus, OP_DUMP_OBJPATH);
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+    exit(EXIT_SUCCESS);
+}
