013595a12e66efed0613c369a19cf1569961cdd6
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-27 07:20:34 -0500

    Add a function to write the dump file
    The dump collection application calls SBE chip-op to collect dump
    data from the SBE. These dump pieces need to be stored in the
    flash to package them later. This commit adds the code for
    writing the dump data to the flash.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I9da73bd91b381b4de7774c1dd189eccdf8f633d5

--- dump/dump_collect.cpp
+++ dump/dump_collect.cpp
@@ -7,11 +7,15 @@ extern "C"
 #include <fmt/core.h>
 #include <libphal.H>
 
+#include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
 #include <sbe_consts.hpp>
+#include <xyz/openbmc_project/Common/File/error.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
 
 #include <cstdint>
 #include <filesystem>
+#include <fstream>
 
 namespace openpower
 {
@@ -19,6 +23,64 @@ namespace dump
 {
 namespace sbe_chipop
 {
+
+void writeDumpFile(const std::filesystem::path& path, const uint32_t id,
+                   const uint8_t clockState, const uint8_t chipPos,
+                   util::DumpDataPtr& dataPtr, const uint32_t len)
+{
+    using namespace phosphor::logging;
+    using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+    namespace fileError = sdbusplus::xyz::openbmc_project::Common::File::Error;
+
+    // Filename format: <dump_id>.SbeDataClocks<On/Off>.node0.proc<number>
+    std::stringstream ss;
+    ss << std::setw(8) << std::setfill('0') << id;
+    std::string clockStr = (clockState == SBE::SBE_CLOCK_ON) ? "On" : "Off";
+
+    // Assuming only node0 is supported now
+    auto filename = ss.str() + ".SbeDataClocks" + clockStr + ".node0.proc" +
+                    std::to_string(chipPos);
+
+    std::filesystem::path dumpPath = path / filename;
+    std::ofstream outfile{dumpPath, std::ios::out | std::ios::binary};
+    if (!outfile.good())
+    {
+        using namespace sdbusplus::xyz::openbmc_project::Common::File::Error;
+        using metadata = xyz::openbmc_project::Common::File::Open;
+        // Unable to open the file for writing
+        auto err = errno;
+        log<level::ERR>(
+            fmt::format(
+                "Error opening file to write dump, errno({}), filepath({})",
+                err, dumpPath.string())
+                .c_str());
+        report<Open>(metadata::ERRNO(err), metadata::PATH(dumpPath.c_str()));
+        // Just return here, so that the dumps collected from other
+        // SBEs can be packaged.
+        return;
+    }
+    outfile.exceptions(std::ifstream::failbit | std::ifstream::badbit |
+                       std::ifstream::eofbit);
+    try
+    {
+        outfile.write(reinterpret_cast<char*>(dataPtr.getData()), len);
+    }
+    catch (std::ofstream::failure& oe)
+    {
+        using namespace sdbusplus::xyz::openbmc_project::Common::File::Error;
+        using metadata = xyz::openbmc_project::Common::File::Write;
+        log<level::ERR>(fmt::format("Failed to write to dump file, "
+                                    "errorMsg({}), error({}), filepath({})",
+                                    oe.what(), oe.code().value(),
+                                    dumpPath.string())
+                            .c_str());
+        report<Write>(metadata::ERRNO(oe.code().value()),
+                      metadata::PATH(dumpPath.c_str()));
+        // Just return here so dumps collected from other SBEs can be
+        // packaged.
+    }
+    outfile.close();
+}
 
 void collectDumpFromSBE(struct pdbg_target* proc,
                         const std::filesystem::path& path, const uint32_t id,
