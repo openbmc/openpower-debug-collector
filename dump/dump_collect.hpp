af1e18ec91d80d118942c8ea51b81d4b29b8729e
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-27 03:26:06 -0500

    Add dump collection application
    Add a standalone application to execute the SBE chip-ops and
    collect the dumps. This standalone application is created
    to isolate the long-running dump collection from the D-Bus
    service,
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: If6f44075d33af20e09a442d7968d235dc6e8ea16

---
+++ dump/dump_collect.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <filesystem>
+
+namespace openpower
+{
+namespace dump
+{
+
+namespace sbe_chipop
+{
+/** @brief The function to orchestrate dump collection from different
+ *  SBEs
+ *  @param type - Type of the dump
+ *  @param id - A unique id assigned to dump to be collected
+ *  @param failingUnit - Chip position of the failing unit
+ *  @param sbeFilePath - Path where the collected dump to be stored
+ */
+void collectDump(const uint8_t type, const uint32_t id,
+                 const uint64_t failingUnit, const std::filesystem::path& path);
+
+/** @brief The function to collect dump from SBE
+ *  @param[in] proc - pdbg_target of the proc containing SBE to collect the
+ * dump.
+ *  @param[in] dumpPath - Path of directory to write the dump files.
+ *  @param[in] id - Id of the dump
+ *  @param[in] type - Type of the dump
+ *  @param[in] clockState - State of the clock while collecting.
+ *  @param[in] chipPos - Position of the chip
+ *  @param[in] failingUnit - Chip position of the failing unit
+ */
+void collectDumpFromSBE(struct pdbg_target* proc,
+                        const std::filesystem::path& path, const uint32_t id,
+                        const uint8_t type, const uint8_t clockState,
+                        const uint8_t chipPos, const uint64_t failingUnit);
+
+} // namespace sbe_chipop
+} // namespace dump
+} // namespace openpower
