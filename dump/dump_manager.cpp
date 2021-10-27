aa9a071768b75096ef0c865521eff952cf54e602
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-27 01:55:17 -0500

    Add a new function to get parameters from D-Bus
    This function will get the required parameters to create the
    dump from the user input. An error will be returned if all
    the necessary parameters are not provided or are invalid.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ib173d120eb895065329167d852ed597ffc4de9f1

---
+++ dump/dump_manager.cpp
@@ -0,0 +1,163 @@
+#include "dump_manager.hpp"
+
+#include "sbe_consts.hpp"
+
+#include <fmt/core.h>
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/log.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+#include <xyz/openbmc_project/Dump/Create/error.hpp>
+
+namespace openpower
+{
+namespace dump
+{
+
+constexpr auto MAX_ERROR_LOG_ID = 0xFFFFFFFF;
+
+// Maximum possible processors in an OpenPOWER system
+constexpr auto MAX_FAILING_UNIT = 0x20;
+
+std::map<std::string, uint8_t> dumpTypeMap = {
+    {"com.ibm.Dump.Create.DumpType.Hostboot", SBE::SBE_DUMP_TYPE_HOSTBOOT},
+    {"com.ibm.Dump.Create.DumpType.Hardware", SBE::SBE_DUMP_TYPE_HARDWARE},
+    {"com.ibm.Dump.Create.DumpType.SBE", SBE::SBE_DUMP_TYPE_SBE}};
+
+void Manager::getParams(const DumpCreateParams& params, DumpParams& dparams)
+{
+    using namespace phosphor::logging;
+    using InvalidArgument =
+        sdbusplus::xyz::openbmc_project::Common::Error::InvalidArgument;
+    using CreateParameters =
+        sdbusplus::com::ibm::Dump::server::Create::CreateParameters;
+    using Argument = xyz::openbmc_project::Common::InvalidArgument;
+
+    // Get dump type
+    auto iter = params.find(
+        sdbusplus::com::ibm::Dump::server::Create::
+            convertCreateParametersToString(CreateParameters::DumpType));
+    if (iter == params.end())
+    {
+        log<level::ERR>("Required argument, dump type is not passed");
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("DUMP_TYPE"),
+                              Argument::ARGUMENT_VALUE("MISSING"));
+    }
+
+    if (std::holds_alternative<std::string>(iter->second))
+    {
+        log<level::ERR>("Invalid argument value is passed for dump type");
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("DUMP_TYPE"),
+                              Argument::ARGUMENT_VALUE("INVALID"));
+    }
+
+    std::string dumpType = std::get<std::string>(iter->second);
+
+    auto dumpTypeIter = dumpTypeMap.find(dumpType);
+    if (dumpTypeIter == dumpTypeMap.end())
+    {
+        log<level::ERR>(
+            fmt::format("Invalid dump type passed dumpType({}) error", dumpType)
+                .c_str());
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("DUMP_TYPE"),
+                              Argument::ARGUMENT_VALUE(dumpType.c_str()));
+    }
+
+    dparams.dumpType = dumpTypeIter->second;
+
+    // get error log id
+    iter = params.find(
+        sdbusplus::com::ibm::Dump::server::Create::
+            convertCreateParametersToString(CreateParameters::ErrorLogId));
+    if (iter == params.end())
+    {
+        log<level::ERR>("Required argument, error log id is not passed");
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("ERROR_LOG_ID"),
+                              Argument::ARGUMENT_VALUE("MISSING"));
+    }
+
+    if (std::holds_alternative<uint64_t>(iter->second))
+    {
+        // Exception will be raised if the input is not uint64
+        log<level::ERR>("An ivalid error log id is passed, setting ");
+        report<InvalidArgument>(Argument::ARGUMENT_NAME("ERROR_LOG_ID"),
+                                Argument::ARGUMENT_VALUE("INVALID INPUT"));
+    }
+
+    dparams.eid = std::get<uint64_t>(iter->second);
+
+    if (dparams.eid > MAX_ERROR_LOG_ID)
+    {
+        // An error will be logged if the error log id is larger than maximum
+        // value and set the error log id as 0.
+        log<level::ERR>(fmt::format("Error log id is greater than maximum({}) "
+                                    "length, setting as 0, errorid({})",
+                                    MAX_ERROR_LOG_ID, dparams.eid)
+                            .c_str());
+        report<InvalidArgument>(
+            Argument::ARGUMENT_NAME("ERROR_LOG_ID"),
+            Argument::ARGUMENT_VALUE(std::to_string(dparams.eid).c_str()));
+        dparams.eid = 0;
+    }
+
+    if ((dparams.dumpType == SBE::SBE_DUMP_TYPE_HARDWARE) ||
+        (dparams.dumpType == SBE::SBE_DUMP_TYPE_SBE))
+    {
+        iter = params.find(sdbusplus::com::ibm::Dump::server::Create::
+                               convertCreateParametersToString(
+                                   CreateParameters::FailingUnitId));
+        if (iter == params.end())
+        {
+            log<level::ERR>("Required argument, failing unit id is not passed");
+            elog<InvalidArgument>(Argument::ARGUMENT_NAME("FAILING_UNIT_ID"),
+                                  Argument::ARGUMENT_VALUE("MISSING"));
+        }
+
+        try
+        {
+            dparams.failingUnit = std::get<uint64_t>(iter->second);
+        }
+        catch (const std::bad_variant_access& e)
+        {
+            // Exception will be raised if the input is not uint64
+            auto err = errno;
+            log<level::ERR>(
+                fmt::format("An invalid failing unit id is passed "
+                            "errorMsg({}), errno({}), errorString({})",
+                            e.what(), err, strerror(err))
+                    .c_str());
+            elog<InvalidArgument>(Argument::ARGUMENT_NAME("FAILING_UNIT_ID"),
+                                  Argument::ARGUMENT_VALUE("INVALID INPUT"));
+        }
+
+        if (dparams.failingUnit > MAX_FAILING_UNIT)
+        {
+            log<level::ERR>(fmt::format("Invalid failing uint id: greater than "
+                                        "maximum number({}): input({})",
+                                        dparams.failingUnit, MAX_FAILING_UNIT)
+                                .c_str());
+            elog<InvalidArgument>(
+                Argument::ARGUMENT_NAME("FAILING_UNIT_ID"),
+                Argument::ARGUMENT_VALUE(
+                    std::to_string(dparams.failingUnit).c_str()));
+        }
+    }
+}
+
+sdbusplus::message::object_path
+    Manager::createDump(DumpCreateParams createParams)
+{
+    using namespace phosphor::logging;
+    DumpParams dumpParams;
+    getParams(createParams, dumpParams);
+    log<level::INFO>(
+        fmt::format(
+            "Request to collect dump type({}), eid({}), failingUnit({})",
+            dumpParams.dumpType, dumpParams.eid, dumpParams.failingUnit)
+            .c_str());
+
+    return sdbusplus::message::object_path();
+}
+} // namespace dump
+} // namespace openpower
