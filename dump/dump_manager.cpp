b8a6ce7d0904c04c4bfddeab5fdbc8fe6d7272a7
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-26 22:32:27 -0500

    Keep a dummy openpower dump manager service
    The dump collection interface is moved to phosphor-
    dump-manager but keeping openpower-dump-manager
    as a proxy of phosphor-dump-manager until the clients
    are moving to new interface for collecting host dumps.
    
    Change-Id: I9e5cd30dd1daadc6fa36fa9ce92293f7235566c6
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

---
+++ dump/dump_manager.cpp
@@ -0,0 +1,141 @@
+#include "dump_manager.hpp"
+
+#include "dump_utils.hpp"
+#include "sbe_consts.hpp"
+
+#include <format>
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <phosphor-logging/log.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+#include <xyz/openbmc_project/Dump/Create/error.hpp>
+
+namespace openpower
+{
+namespace dump
+{
+
+constexpr auto DUMP_CREATE_IFACE = "xyz.openbmc_project.Dump.Create";
+
+constexpr auto ERROR_DUMP_DISABLED =
+    "xyz.openbmc_project.Dump.Create.Error.Disabled";
+constexpr auto ERROR_DUMP_QUOTA_EXCEEDED =
+    "xyz.openbmc_project.Dump.Create.Error.QuotaExceeded";
+constexpr auto ERROR_DUMP_NOT_ALLOWED =
+    "xyz.openbmc_project.Common.Error.NotAllowed";
+
+std::unordered_map<std::string, std::string> dumpTypeMap = {
+    {"com.ibm.Dump.Create.DumpType.Hostboot",
+     "/xyz/openbmc_project/dump/hostboot"},
+    {"com.ibm.Dump.Create.DumpType.Hardware",
+     "/xyz/openbmc_project/dump/hardware"},
+    {"com.ibm.Dump.Create.DumpType.SBE", "/xyz/openbmc_project/dump/sbe"}};
+
+sdbusplus::message::object_path
+    Manager::createDump(DumpCreateParams createParams)
+{
+    using namespace phosphor::logging;
+    sdbusplus::message::object_path newDumpPath;
+    auto dumpPath = extractDumpPath(createParams);
+    log<level::INFO>(
+        std::format("Request to collect dump({})", dumpPath).c_str());
+    try
+    {
+        // Pass empty create parameters since no additional parameters
+        // are needed.
+        auto dumpManager = util::getService(bus, DUMP_CREATE_IFACE, dumpPath);
+        auto method = bus.new_method_call(dumpManager.c_str(), dumpPath.c_str(),
+                                          DUMP_CREATE_IFACE, "CreateDump");
+        method.append(createParams);
+        auto response = bus.call(method);
+        response.read(newDumpPath);
+    }
+    catch (const sdbusplus::exception::SdBusError& e)
+    {
+        log<level::ERR>(
+            std::format("D-Bus call exception, errorMsg({})", e.what())
+                .c_str());
+        if (e.name() == ERROR_DUMP_DISABLED)
+        {
+            elog<sdbusplus::xyz::openbmc_project::Dump::Create::Error::
+                     Disabled>();
+        }
+        if (e.name() == ERROR_DUMP_QUOTA_EXCEEDED)
+        {
+            using DumpQuotaExceeded = sdbusplus::xyz::openbmc_project::Dump::
+                Create::Error::QuotaExceeded;
+            using Reason =
+                xyz::openbmc_project::Dump::Create::QuotaExceeded::REASON;
+            elog<DumpQuotaExceeded>(Reason(e.description()));
+        }
+        if (e.name() == ERROR_DUMP_NOT_ALLOWED)
+        {
+            using DumpNotAllowed =
+                sdbusplus::xyz::openbmc_project::Common::Error::NotAllowed;
+            using Reason = xyz::openbmc_project::Common::NotAllowed::REASON;
+            elog<DumpNotAllowed>(Reason(e.description()));
+        }
+        else
+        {
+            // re-throw exception
+            throw;
+        }
+    }
+    catch (const std::exception& e)
+    {
+        log<level::ERR>(
+            std::format("Failed to get dump id,invalid dump entry path({})",
+                        std::string(newDumpPath))
+                .c_str());
+        throw;
+    }
+    return newDumpPath;
+}
+
+std::string Manager::extractDumpPath(DumpCreateParams& params)
+{
+    using namespace phosphor::logging;
+    using InvalidArgument =
+        sdbusplus::xyz::openbmc_project::Common::Error::InvalidArgument;
+    using CreateParameters =
+        sdbusplus::com::ibm::Dump::server::Create::CreateParameters;
+    using Argument = xyz::openbmc_project::Common::InvalidArgument;
+
+    // Get dump type
+    auto iter = params.find(
+        sdbusplus::com::ibm::Dump::server::Create::
+            convertCreateParametersToString(CreateParameters::DumpType));
+    if (iter == params.end())
+    {
+        log<level::ERR>("Required argument, dump type is missing");
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("DUMP_TYPE"),
+                              Argument::ARGUMENT_VALUE("MISSING"));
+    }
+
+    if (!std::holds_alternative<std::string>(iter->second))
+    {
+        log<level::ERR>("Invalid argument value is passed for dump type");
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("DUMP_TYPE"),
+                              Argument::ARGUMENT_VALUE("INVALID"));
+    }
+
+    auto dumpTypeStr = std::get<std::string>(iter->second);
+    params.erase(iter);
+
+    auto dumpTypeIter = dumpTypeMap.find(dumpTypeStr);
+    if (dumpTypeIter == dumpTypeMap.end())
+    {
+        log<level::ERR>(
+            std::format("Invalid dump type passed dumpType({}) error",
+                        dumpTypeStr)
+                .c_str());
+        elog<InvalidArgument>(Argument::ARGUMENT_NAME("DUMP_TYPE"),
+                              Argument::ARGUMENT_VALUE(dumpTypeStr.c_str()));
+    }
+
+    return dumpTypeIter->second;
+}
+
+} // namespace dump
+} // namespace openpower
---
+++ dump/dump_manager.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <com/ibm/Dump/Create/server.hpp>
+#include <sdbusplus/bus.hpp>
+#include <xyz/openbmc_project/Dump/Create/server.hpp>
+
+namespace openpower
+{
+namespace dump
+{
+using DumpCreateParams =
+    std::map<std::string, std::variant<std::string, uint64_t>>;
+
+using CreateIface = sdbusplus::server::object::object<
+    sdbusplus::com::ibm::Dump::server::Create,
+    sdbusplus::xyz::openbmc_project::Dump::server::Create>;
+
+/** @class Manager
+ *  @brief Dump  manager class
+ *  @details A concrete implementation for the
+ *  xyz::openbmc_project::Dump::server::Create D-Bus API.
+ */
+class Manager : public CreateIface
+{
+  public:
+    Manager() = delete;
+    Manager(const Manager&) = default;
+    Manager& operator=(const Manager&) = delete;
+    Manager(Manager&&) = delete;
+    Manager& operator=(Manager&&) = delete;
+    virtual ~Manager() = default;
+
+    /** @brief Constructor to put object onto bus at a dbus path.
+     *  @param[in] bus - Bus to attach to.
+     *  @param[in] path - Path of the service.
+     */
+    Manager(sdbusplus::bus::bus& bus, const char* path) :
+        CreateIface(bus, path), bus(bus)
+    {}
+
+    /** @brief Implementation for createDump
+     *  Method to request a host dump when there is an error related to
+     *  host hardware or firmware.
+     *  @param[in] params - Parameters for creating the dump.
+     *
+     *  @return object_path - The object path of the new dump entry.
+     */
+    sdbusplus::message::object_path
+        createDump(DumpCreateParams createParams) override;
+
+  private:
+    /** @brief Get the dump path from dump type
+     *  @param[in] params - Parameters for creating the dump.
+     *
+     *  @return D-Bus path for creating dump
+     */
+    std::string extractDumpPath(DumpCreateParams& param);
+
+    /** @brief sdbusplus DBus bus connection. */
+    sdbusplus::bus::bus& bus;
+};
+
+} // namespace dump
+} // namespace openpower
