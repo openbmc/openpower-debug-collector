31f1c04c6cbe4627b97838b213838182eab4b56a
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-26 23:00:52 -0500

    Add dump collection preparation.
    This commit adds a utility function for preparing the dump collection
    which includes creating directories and adding error log files.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I1bf3150e74bdf04b37c784d4595c30f6bfbdf802

--- dump/dump_utils.cpp
+++ dump/dump_utils.cpp
@@ -2,8 +2,12 @@
 
 #include <fmt/core.h>
 
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
 #include <phosphor-logging/log.hpp>
+#include <xyz/openbmc_project/Common/File/error.hpp>
 
+#include <fstream>
 #include <string>
 
 namespace openpower
@@ -48,6 +52,62 @@ std::string getService(sdbusplus::bus::bus& bus, const std::string& intf,
                                     ex.what(), path, intf)
                             .c_str());
         throw;
+    }
+}
+
+void prepareCollection(const std::filesystem::path& dumpPath,
+                       const std::string& errorLogId)
+{
+    using namespace sdbusplus::xyz::openbmc_project::Common::File::Error;
+    try
+    {
+        std::filesystem::create_directories(dumpPath);
+        auto elogPath = dumpPath.parent_path();
+        elogPath /= "ErrorLog";
+        std::ofstream outfile{elogPath, std::ios::out | std::ios::binary};
+        if (!outfile.good())
+        {
+            using metadata = xyz::openbmc_project::Common::File::Open;
+            // Unable to open the file for writing
+            auto err = errno;
+            log<level::ERR>(
+                fmt::format("Error opening file to write errorlog id, "
+                            "errno({}), filepath({})",
+                            err, dumpPath.string())
+                    .c_str());
+            // Report the error and continue collection even if the error log id
+            // cannot be added
+            report<Open>(metadata::ERRNO(err),
+                         metadata::PATH(dumpPath.c_str()));
+        }
+        else
+        {
+            outfile.exceptions(std::ifstream::failbit | std::ifstream::badbit |
+                               std::ifstream::eofbit);
+            outfile << errorLogId;
+            outfile.close();
+        }
+    }
+    catch (std::filesystem::filesystem_error& e)
+    {
+        log<level::ERR>(fmt::format("Error creating dump directories, path({})",
+                                    dumpPath.string())
+                            .c_str());
+        throw;
+    }
+    catch (std::ofstream::failure& oe)
+    {
+        using metadata = xyz::openbmc_project::Common::File::Write;
+        // If there is an error commit the error and continue.
+        log<level::ERR>(fmt::format("Failed to write errorlog id to file, "
+                                    "errorMsg({}), error({}), filepath({})",
+                                    oe.what(), oe.code().value(),
+                                    dumpPath.string())
+                            .c_str());
+        // Report the error and continue with dump collection
+        // even if the error log id cannot be written to the file.
+        report<Write>(metadata::ERRNO(oe.code().value()),
+                      metadata::PATH(dumpPath.c_str()));
     }
 }
 
