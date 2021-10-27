46fbdaffe7ffa306e763b9612b91f17a4fc56917
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-26 22:43:02 -0500

    Add D-Bus utilities
    Add basic D-Bus utilities for dump collector application
    like getting the service and setting up the property.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I54619def549738df6d4cb3fb3e7136490106e884

---
+++ dump/dump_utils.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <sdbusplus/server.hpp>
+
+#include <map>
+#include <string>
+#include <variant>
+namespace openpower
+{
+namespace dump
+{
+namespace util
+{
+
+using DumpCreateParams =
+    std::map<std::string, std::variant<std::string, uint64_t>>;
+
+/**
+ * @brief Get DBUS service for input interface via mapper call
+ *
+ * @param[in] bus -  DBUS Bus Object
+ * @param[in] intf - DBUS Interface
+ * @param[in] path - DBUS Object Path
+ *
+ * @return distinct dbus name for input interface/path
+ **/
+std::string getService(sdbusplus::bus::bus& bus, const std::string& intf,
+                       const std::string& path);
+
+/**
+ * @brief Set the property value based on the inputs
+ *
+ * @param[in] interface - the interface the property is on
+ * @param[in] propertName - the name of the property
+ * @param[in] path - the D-Bus path
+ * @param[in] service - the D-Bus service
+ * @param[in] bus - the D-Bus object
+ * @param[in] value - the value to set the property to
+ */
+template <typename T>
+void setProperty(const std::string& interface, const std::string& propertyName,
+                 const std::string& path, sdbusplus::bus::bus& bus,
+                 const T& value)
+{
+    constexpr auto PROPERTY_INTF = "org.freedesktop.DBus.Properties";
+
+    auto service = getService(bus, interface, path);
+    auto method = bus.new_method_call(service.c_str(), path.c_str(),
+                                      PROPERTY_INTF, "Set");
+    method.append(interface, propertyName, value);
+    auto reply = bus.call(method);
+}
+
+} // namespace util
+} // namespace dump
+} // namespace openpower
