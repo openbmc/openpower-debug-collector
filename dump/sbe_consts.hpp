77ccd2048e76ed17cfaf45ada55c1d30ab1cdb9f
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2021-10-27 03:26:06 -0500

    Add dump collection application
    Add a standalone application to execute the SBE chip-ops and
    collect the dumps. This standalone application is created
    to isolate the long-running dump collection from the D-Bus
    service,
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: If6f44075d33af20e09a442d7968d235dc6e8ea16

---
+++ dump/sbe_consts.hpp
@@ -0,0 +1,24 @@
+#pragma once
+namespace openpower
+{
+namespace dump
+{
+namespace SBE
+{
+// Dump type to the sbe_dump chipop
+constexpr auto SBE_DUMP_TYPE_HOSTBOOT = 0x5;
+constexpr auto SBE_DUMP_TYPE_HARDWARE = 0x1;
+
+// SBE dump type
+constexpr auto SBE_DUMP_TYPE_SBE = 0xA;
+
+// Clock state requested
+// Collect the dump with clocks on
+constexpr auto SBE_CLOCK_ON = 0x1;
+
+// Collect the dumps with clock off
+constexpr auto SBE_CLOCK_OFF = 0x2;
+
+} // namespace SBE
+} // namespace dump
+} // namespace openpower
