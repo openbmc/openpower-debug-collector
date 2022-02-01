9386e7dc42a62bc0edb5ec0688769e3f14453651
Shantappa Teekappanavar <sbteeks@yahoo.com>
2022-02-01 09:28:10 -0600

    Callout support for SBE when watchdog times out
    This commit provides additional callout support for SBE when the
    host does not respond within watchdog timeout interval during SBE
    boot window.
    
    Testing:
    Steps used:
     1. obmcutil poweroff
     2. istep -s0
     3. systemctl start org.open_power.Dump.Manager.service
     4. systemctl start openpower-debug-collector-watchdog@0.service
     5. Check the journal log, and get the PEL id to print PEL info
    
    PEL info:
        "Callout Section": {
            "Callout Count":        "2",
            "Callouts": [{
                "FRU Type":         "Maintenance Procedure Required",
                "Priority":         "Mandatory, replace all with this type
    as a unit",
                "Procedure":        "BMC0002"
            }, {
                "FRU Type":         "Normal Hardware FRU",
                "Priority":         "Medium Priority",
                "Location Code":    "xxxxxxxxxxxxx",
                "Part Number":      "xxxxx",
                "CCIN":             "xxxx",
                "Serial Number":    "xxxxxxx"
            }
    ...
    "User Data 2": {
        "Section Version": "1",
        "Sub-section type": "1",
        "Created by": "0x2000",
        "Data": [
            {
                "Priority": "H",
                "Procedure": "BMC0002"
            },
            {
                "Deconfigured": false,
                "Guarded": false,
                "LocationCode": "Ufcs-xx-xxx",
                "Priority": "M"
            }
        ]
    }
    
    Signed-off-by: Shantappa Teekappanavar <sbteeks@yahoo.com>
    Change-Id: I5e182cb415a807d97c98812a6713905d39fdbc9a

---
+++ watchdog/ffdc_file.cpp
@@ -0,0 +1,106 @@
+#include "ffdc_file.hpp"
+
+#include <errno.h>     // for errno
+#include <fcntl.h>     // for open()
+#include <string.h>    // for strerror()
+#include <sys/stat.h>  // for open()
+#include <sys/types.h> // for open()
+
+#include <stdexcept>
+#include <string>
+
+namespace watchdog
+{
+namespace dump
+{
+
+FFDCFile::FFDCFile(const json& calloutDataObject) :
+    calloutData(calloutDataObject.dump())
+{
+    prepareFFDCFile();
+}
+
+FFDCFile::~FFDCFile()
+{
+    remove();
+}
+
+void FFDCFile::prepareFFDCFile()
+{
+    createFile();
+    writeCalloutData();
+    seekFilePosition();
+}
+
+void FFDCFile::createFile()
+{
+    // Open the temporary file for both reading and writing
+    int fd = open(tempFile.getPath().c_str(), O_RDWR);
+    if (fd == -1)
+    {
+        throw std::runtime_error{std::string{"Unable to open FFDC file: "} +
+                                 strerror(errno)};
+    }
+
+    // Store file descriptor in FileDescriptor object
+    descriptor.set(fd);
+}
+
+void FFDCFile::writeCalloutData()
+{
+    ssize_t rc =
+        write(getFileDescriptor(), calloutData.c_str(), calloutData.size());
+
+    if (rc == -1)
+    {
+        log<level::ERR>(fmt::format("Failed to write callout info "
+                                    "in file({}), errorno({}), errormsg({})",
+                                    tempFile.getPath().c_str(), errno,
+                                    strerror(errno))
+                            .c_str());
+        throw std::runtime_error("Failed to write phalPELCallouts info");
+    }
+    else if (rc != static_cast<ssize_t>(calloutData.size()))
+    {
+        log<level::WARNING>(fmt::format("Could not write all callout "
+                                        "info in file({}), written byte({}) "
+                                        "and total byte({})",
+                                        tempFile.getPath().c_str(), rc,
+                                        calloutData.size())
+                                .c_str());
+    }
+}
+
+void FFDCFile::seekFilePosition()
+{
+    int rc = lseek(getFileDescriptor(), 0, SEEK_SET);
+
+    if (rc == -1)
+    {
+        log<level::ERR>(
+            fmt::format("Failed to seek file postion to the beginning"
+                        "in file({}), errorno({}) "
+                        "and errormsg({})",
+                        tempFile.getPath().c_str(), errno, strerror(errno))
+                .c_str());
+        throw std::runtime_error(
+            "Failed to seek file postion to the beginning of the file");
+    }
+}
+
+void FFDCFile::remove()
+{
+    // Close file descriptor.  Does nothing if descriptor was already closed.
+    if (descriptor.close() == -1)
+    {
+        log<level::ERR>(fmt::format("Unable to close FFDC file: errormsg({})",
+                                    strerror(errno))
+                            .c_str());
+    }
+
+    // Delete temporary file.  Does nothing if file was already deleted.
+    tempFile.remove();
+}
+
+} // namespace dump
+} // namespace watchdog
---
+++ watchdog/utils.cpp
@@ -0,0 +1,80 @@
+#include "utils.hpp"
+
+#include <errno.h>  // for errno
+#include <stdlib.h> // for mkstemp()
+#include <string.h> // for strerror()
+#include <unistd.h> // for close()
+
+#include <stdexcept>
+#include <string>
+
+namespace watchdog
+{
+namespace dump
+{
+
+TemporaryFile::TemporaryFile()
+{
+    // Build template path required by mkstemp()
+    std::string templatePath =
+        fs::temp_directory_path() / "openpower-debug-collector-XXXXXX";
+
+    // Generate unique file name, create file, and open it.  The XXXXXX
+    // characters are replaced by mkstemp() to make the file name unique.
+    int fd = mkstemp(templatePath.data());
+    if (fd == -1)
+    {
+        throw std::runtime_error{
+            std::string{"Unable to create temporary file: "} + strerror(errno)};
+    }
+
+    // Store path to temporary file
+    path = templatePath;
+
+    // Close file descriptor
+    if (close(fd) == -1)
+    {
+        // Save errno value; will likely change when we delete temporary file
+        int savedErrno = errno;
+
+        // Delete temporary file.  The destructor won't be called because the
+        // exception below causes this constructor to exit without completing.
+        remove();
+
+        throw std::runtime_error{
+            std::string{"Unable to close temporary file: "} +
+            strerror(savedErrno)};
+    }
+}
+
+TemporaryFile& TemporaryFile::operator=(TemporaryFile&& file)
+{
+    // Verify not assigning object to itself (a = std::move(a))
+    if (this != &file)
+    {
+        // Delete temporary file owned by this object
+        remove();
+
+        // Move temporary file path from other object, transferring ownership
+        path = std::move(file.path);
+
+        // Clear path in other object; after move path is in unspecified state
+        file.path.clear();
+    }
+    return *this;
+}
+
+void TemporaryFile::remove()
+{
+    if (!path.empty())
+    {
+        // Delete temporary file from file system
+        fs::remove(path);
+
+        // Clear path to indicate file has been deleted
+        path.clear();
+    }
+}
+
+} // namespace dump
+} // namespace watchdog
