6f7ed539385e6684e3dfd3854880b6ce981a9e12
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2022-03-23 03:54:32 -0500

    Request a BMC dump at the end of system dump collection
    An additional BMC dump will help debugging the host error
    because the journal and the error log on the BMC can help
    debugging the host errors. Add a new method to request
    BMC dump at the end of the collection
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Id29d3044306f23842242b62697c7cdc397481ec7

--- dump/dump_utils.cpp
+++ dump/dump_utils.cpp
@@ -101,6 +101,32 @@ void monitorDump(const std::string& path, const uint32_t timeout)
     }
 }
 
+void requestBMCDump()
+{
+    constexpr auto path = "/xyz/openbmc_project/dump/bmc";
+    constexpr auto interface = "xyz.openbmc_project.Dump.Create";
+    constexpr auto function = "CreateDump";
+
+    log<level::INFO>("Initiating BMC dump");
+    auto bus = sdbusplus::bus::new_default();
+    try
+    {
+        auto service = getService(bus, interface, path);
+        auto method =
+            bus.new_method_call(service.c_str(), path, interface, function);
+        std::map<std::string, std::variant<std::string, uint64_t>> createParams;
+        method.append(createParams);
+
+        bus.call_noreply(method);
+    }
+    catch (const std::exception& e)
+    {
+        log<level::ERR>(
+            fmt::format("BMC dump creation reqest failed, error({})", e.what())
+                .c_str());
+    }
+}
+
 void requestSBEDump(const uint32_t failingUnit, const uint32_t eid)
 {
     log<level::INFO>(fmt::format("Requesting Dump PEL({}) chip position({})",
