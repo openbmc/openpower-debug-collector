fcab1f53c0d747e9625af15d9f6565d6b64b4e32
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2023-04-04 06:38:34 -0500

    Keep a dummy openpower dump manager service
    The dump collection interface is moved to phosphor-
    dump-manager but keeping openpower-dump-manager
    as a proxy of phosphor-dump-manager until the clients
    are moving to new interface for collecting host dumps.
    
    Change-Id: I9e5cd30dd1daadc6fa36fa9ce92293f7235566c6
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump/dump_manager.hpp
+++ dump/dump_manager.hpp
@@ -2,8 +2,6 @@
 
 #include <com/ibm/Dump/Create/server.hpp>
 #include <sdbusplus/bus.hpp>
-#include <sdeventplus/source/child.hpp>
-#include <sdeventplus/source/event.hpp>
 #include <xyz/openbmc_project/Dump/Create/server.hpp>
 
 #include <map>
@@ -12,35 +10,12 @@ namespace openpower
 {
 namespace dump
 {
-/* Need a custom deleter for freeing up sd_event */
-struct EventDeleter
-{
-    void operator()(sd_event* event) const
-    {
-        event = sd_event_unref(event);
-    }
-};
-
-using EventPtr = std::unique_ptr<sd_event, EventDeleter>;
-
 using DumpCreateParams =
     std::map<std::string, std::variant<std::string, uint64_t>>;
-
-/** @struct DumpParams
- *  @brief Parameters for dump
- */
-struct DumpParams
-{
-    uint8_t dumpType;     // Type of the dump
-    uint64_t eid;         // Eid associated with dump
-    uint64_t failingUnit; // Unit failed
-    uint32_t id;          // Dump id
-};
 
 using CreateIface = sdbusplus::server::object::object<
     sdbusplus::com::ibm::Dump::server::Create,
     sdbusplus::xyz::openbmc_project::Dump::server::Create>;
-using ::sdeventplus::source::Child;
 
 /** @class Manager
  *  @brief Dump  manager class
@@ -60,10 +35,9 @@ class Manager : public CreateIface
     /** @brief Constructor to put object onto bus at a dbus path.
      *  @param[in] bus - Bus to attach to.
      *  @param[in] path - Path of the service.
-     *  @param[in] event - sd event handler.
      */
-    Manager(sdbusplus::bus::bus& bus, const char* path, const EventPtr& event) :
-        CreateIface(bus, path), bus(bus), eventLoop(event.get())
+    Manager(sdbusplus::bus::bus& bus, const char* path) :
+        CreateIface(bus, path), bus(bus)
     {}
 
     /** @brief Implementation for createDump
@@ -73,30 +47,19 @@ class Manager : public CreateIface
      *
      *  @return object_path - The object path of the new dump entry.
      */
-    sdbusplus::message::object_path createDump(DumpCreateParams) override;
+    sdbusplus::message::object_path
+        createDump(DumpCreateParams createParams) override;
 
   private:
-    /** @brief Get the dump params from arguments
+    /** @brief Get the dump path from dump type
      *  @param[in] params - Parameters for creating the dump.
-     *  @param[out] dparams - Dump parameter struct with values
-     */
-    void getParams(const DumpCreateParams& params, DumpParams& dparams);
-
-    /** @brief Create the entry for the dump
-     *  @param[in] dparams - Dump parameter struct with values
      *
-     *   @return object_path - The object path of the new dump entry
+     *  @return D-Bus path for creating dump
      */
-    sdbusplus::message::object_path createDumpEntry(DumpParams& dparams);
+    std::string extractDumpPath(DumpCreateParams& param);
 
     /** @brief sdbusplus DBus bus connection. */
     sdbusplus::bus::bus& bus;
-
-    /** @brief sdbusplus Dump event loop */
-    EventPtr eventLoop;
-
-    /** @brief map of SDEventPlus child pointer added to event loop */
-    std::map<pid_t, std::unique_ptr<Child>> childPtrMap;
 };
 
 } // namespace dump
