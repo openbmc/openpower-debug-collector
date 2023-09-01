b9142c077d45fbe7fbf4b1acea259b57319a7c05
Patrick Williams <patrick@stwcx.xyz>
2023-09-01 15:08:34 -0500

    elog-errors: remove generated file
    Move the error YAML definition to phosphor-dbus-interfaces and rely
    on the real elog-errors.hpp from phosphor-logging rather than a
    generated one.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I944430fc63e2e1ea169947ae43d061f19458f9fc

--- watchdog_timeout.cpp
+++ watchdog_timeout.cpp
@@ -18,9 +18,8 @@ extern "C"
 
 #include <format>
 #else
-#include "org/open_power/Host/Boot/error.hpp"
-#include "phosphor-logging/elog-errors.hpp"
-
+#include <org/open_power/Host/Boot/error.hpp>
+#include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/elog.hpp>
 #endif
 
