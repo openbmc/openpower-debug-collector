05cec149cbc06b34ace9adf7585ff3432fb7dfdc
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2024-04-01 09:53:13 -0500

    Support for HW dump from Odyssey
    This commit adds support for Hardware Dump from
    Odyssey based OCMB chips.
    
    Change-Id: Iea70912f9fef9c337ba5ca7c508728586be25af8
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

--- dump/sbe_type.cpp
+++ dump/sbe_type.cpp
@@ -7,6 +7,10 @@ const std::map<SBETypes, SBEAttributes> sbeTypeAttributes = {
     {SBETypes::PROC,
      {"proc", "/xyz/openbmc_project/dump/sbe",
       "org.open_power.Processor.Error.SbeChipOpTimeout",
-      "org.open_power.Processor.Error.SbeChipOpFailure"}}};
+      "org.open_power.Processor.Error.SbeChipOpFailure"}},
+    {SBETypes::OCMB,
+     {"ocmb", "/xyz/openbmc_project/dump/msbe",
+      "org.open_power.OCMB.Error.SbeChipOpTimeout",
+      "org.open_power.OCMB.Error.SbeChipOpFailure"}}};
 
 } // namespace openpower::dump
