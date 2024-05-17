5c448158e6f3b8106ae37381418e5a5d0daf1f35
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2024-05-16 23:01:32 -0500

    Add Dump Monitoring Support
    This commit introduces the DumpMonitor class to monitor DBus signals
    for dump creation and manage the collection process. It aims to
    abstract the complexities of dump collection from the
    phosphor-dump-collector and consolidate system-specific handling
    within the openpower-dump-collector.
    
    Rationale:
    - By moving dump collection responsibilities to the
      openpower-dump-collector, we ensure that the
      phosphor-dump-collector` remains focused solely on hosting dump
      entries and interacting with external interfaces.
    - The openpower-dump-collector now handles all system-specific
      dump collection, reducing potential impacts on the
      phosphor-dump-collector.
    - Simplified Management: This change simplifies the overall management
      of dumps by centralizing dump collection logic in one place.
    
    Key Changes:
    1. A new DumpMonitor class monitors DBus signals for dump creation.
    2. The executeCollectionScript function handles the actual dump
       collection and copies the dump to a specified location for the
       phosphor-dump-manager to pick up.
    3. Once the dump is copied to the specific folder, the phosphor dump
       manager will pick up the file and update its status.
    3. In the case of collection failure, the monitor updates the progress
       status to indicate that the dump collection failed.
    
    Change-Id: Id5bac5f3fbc8ef7a4bd9e93ac91796206b3cfda9
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>

---
+++ dump/dump_monitor.hpp
@@ -0,0 +1,137 @@
+#pragma once
+
+#include "dump_utils.hpp"
+#include "sbe_consts.hpp"
+
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/bus/match.hpp>
+
+#include <iostream>
+#include <string>
+#include <variant>
+
+namespace openpower::dump
+{
+
+using PropertyMap = std::map<std::string, std::variant<uint32_t, std::string>>;
+using InterfaceMap = std::map<std::string, PropertyMap>;
+/**
+ * @class DumpMonitor
+ * @brief Monitors DBus signals for dump creation and handles them.
+ */
+class DumpMonitor
+{
+  public:
+    /**
+     * @brief Constructor for DumpMonitor.
+     * Initializes the DBus connection and signal match for monitoring dump
+     * creation.
+     */
+    DumpMonitor() :
+        bus(sdbusplus::bus::new_default()),
+        match(std::make_unique<sdbusplus::bus::match_t>(
+            bus,
+            sdbusplus::bus::match::rules::type::signal() +
+                sdbusplus::bus::match::rules::interface(
+                    "org.freedesktop.DBus.ObjectManager") +
+                sdbusplus::bus::match::rules::member("InterfacesAdded") +
+                sdbusplus::bus::match::rules::path_namespace(
+                    "/xyz/openbmc_project/dump") +
+                sdbusplus::bus::match::rules::sender(
+                    "xyz.openbmc_project.Dump.Manager"),
+            [this](sdbusplus::message_t& msg) { handleDBusSignal(msg); }))
+    {}
+
+    /**
+     * @brief Runs the monitor to continuously listen for DBus signals.
+     */
+    void run()
+    {
+        while (true)
+        {
+            bus.process_discard();
+            bus.wait();
+        }
+    }
+
+  private:
+    sdbusplus::bus::bus bus;
+    std::vector<std::string> monitoredInterfaces = {
+        "com.ibm.Dump.Entry.Hardware", "com.ibm.Dump.Entry.Hostboot",
+        "com.ibm.Dump.Entry.SBE"};
+    std::unique_ptr<sdbusplus::bus::match::match> match;
+
+    /**
+     * @brief Handles the received DBus signal for dump creation.
+     * @param[in] msg - The DBus message received.
+     */
+    void handleDBusSignal(sdbusplus::message::message& msg);
+
+    /**
+     * @brief Checks if the dump creation is in progress.
+     * @param[in] interfaces - The map of interfaces and their properties.
+     * @return True if the dump is in progress, false otherwise.
+     */
+    inline bool isInProgress(const InterfaceMap& interfaces)
+    {
+        auto progressIt =
+            interfaces.find("xyz.openbmc_project.Common.Progress");
+        if (progressIt != interfaces.end())
+        {
+            auto statusIt = progressIt->second.find("Status");
+            if (statusIt != progressIt->second.end())
+            {
+                std::string status = std::get<std::string>(statusIt->second);
+                return status ==
+                       "xyz.openbmc_project.Common.Progress.OperationStatus.InProgress";
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @brief Executes the script to collect the dump.
+     * @param[in] path - The object path of the dump entry.
+     * @param[in] properties - The properties of the dump entry.
+     */
+    void executeCollectionScript(const sdbusplus::message::object_path& path,
+                                 const PropertyMap& properties);
+
+    /**
+     * @brief Updates the progress status of the dump.
+     * @param[in] path - The object path of the dump entry.
+     * @param[in] status - The status to be set.
+     */
+    void updateProgressStatus(const std::string& path,
+                              const std::string& status);
+
+    /**
+     * @brief Gets the dump type from the dump ID.
+     * @param[in] id - The dump ID.
+     * @return The dump type.
+     */
+    inline uint32_t getDumpTypeFromId(uint32_t id)
+    {
+        using namespace openpower::dump::SBE;
+        uint8_t type = (id >> 28) & 0xF;
+        if (type == 0)
+        {
+            return SBE_DUMP_TYPE_HARDWARE;
+        }
+        else if (type == 2)
+        {
+            return SBE_DUMP_TYPE_HOSTBOOT;
+        }
+        else if (type == 3)
+        {
+            return SBE_DUMP_TYPE_SBE;
+        }
+        return 0;
+    }
+};
+
+} // namespace openpower::dump
